bcf92c92a93dee8e644fcf5c6f231a07c3063b22
pqab <pqab.annie@gmail.com>
2019-02-12 01:18:11 +0800

    Initial commit

---
+++ src/test/java/dotterbear/service/config/server/ServiceConfigServerApplicationTests.java
@@ -0,0 +1,17 @@
+package dotterbear.service.config.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
