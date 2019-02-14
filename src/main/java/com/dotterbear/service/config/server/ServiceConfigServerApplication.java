989fc4cece3c07dece84248d9cc1b8cfcf1983f5
kan01234 <safghjkl@gmail.com>
2019-02-14 14:09:34 +0800

    add eureka client config

--- src/main/java/com/dotterbear/service/config/server/ServiceConfigServerApplication.java
+++ src/main/java/com/dotterbear/service/config/server/ServiceConfigServerApplication.java
@@ -1,4 +1,4 @@
-package dotterbear.service.config.server;
+package com.dotterbear.service.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
